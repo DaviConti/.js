7382d8fb7f03b5d655f947ff708349f070912399
Davi_conti <davi_conti@estudante.sesisenai.org.br>
2025-03-20 09:45:57 -0300

    add-matematica simples

--- atividade.js
+++ atividade.js
@@ -13,13 +13,30 @@ function verificarPar(numero) {
 
  */
 
+ /*
  console.log ("Operações matamática simple");
  let num1 = parseFloat(prompt('digite um num1'));
  let num2 = parseFloat(prompt('digite um num1'));
  function matematica(num1, num2) {
 
-    num1 + num2
+    const soma = num1 + num2;
+    const subtracao = num1 - num2;
+    const somultiplicacaoma = num1 * num2;
+
+    console.log(soma)
+    console.log(subtracao)
+    console.log(somultiplicacaoma)
+
+    
+    if(num2 ===0){
+        console.log("não da pra dividor com zero")
+    }else{ soma
+        const divicao = num1 / num2;
+        console.log(divicao)
+    }
   
  }
 
- matematica(numero);
+ matematica(num1, num2);
+
+ */
